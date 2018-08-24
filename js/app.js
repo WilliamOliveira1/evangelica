158393b9a3720bf03e53a8c8fadaceddfd3709be
William da Silva Oliveira <1704639@aluno.univesp.br>
2018-08-24 08:59:46 -0300

    vídeo background, caurosel and logo image.
    Add vídeo background, stop caurosel auto slide and change the title texto to image.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,31 @@
-    //Show a modal when the page loads
-    $("#fotos").click(function() {
-        $('#myModal').modal('show');
-    });
+//Show a modal when the page loads
+$("#fotos").click(function() {
+    $('#myModal').modal('show');
+});
 
-    $("#videos").click(function() {
-        $('#myModal1').modal('show');
-    });
+$("#videos").click(function() {
+    $('#myModal1').modal('show');
+});
 
-    if (window.innerWidth < 645) {
-        $('#fotos').removeClass('invisible');
+if (window.innerWidth < 645) {
+    $('#fotos').removeClass('invisible');
+}
+
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play";
     }
+}
+$(function() {
+    $('#carouselExampleIndicators').carousel({
+        interval: 2000000
+    });
+
+});
